7486b73fcf4fee2bba016ba231362184b9072cda
Yaroslav Kolomiets <yaroslavkolomiets1@gmail.com>
2025-11-21 16:23:39 +0200

    Add all homework tasks

---
+++ src/main/java/homework/Calculator.java
@@ -0,0 +1,12 @@
+package homework;
+
+public class Calculator {
+public  static int add(int a, int b) {
+    return a+b;
+}
+//Метод main
+    public static void  main(String[] args) {
+    int result = add(1,2);
+    System.out.println("Результат" + result);
+    }
+}
---
+++ src/main/java/homework/Student.java
@@ -0,0 +1,12 @@
+package homework;
+
+    public class Student {
+
+        public static void main(String[] args) {
+            String[] students = {"Андрій", "Анна", "Олег", "Ірина", "Наталія"};
+            for (String student : students) {
+                System.out.println(student);
+            }
+        }
+    }
+
