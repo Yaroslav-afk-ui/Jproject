9bb668345848645e19b6547566dae6c175aa7731
Yaroslav Kolomiets <yaroslavkolomiets1@gmail.com>
2025-11-21 16:59:36 +0200

    TownList task

---
+++ src/main/java/homework/TownList.java
@@ -0,0 +1,15 @@
+package homework;
+import java.util.ArrayList;
+
+public class TownList {
+    public static void main(String[] args) {
+
+        // Створюємо список з трьома містами
+        ArrayList<String> cities = new ArrayList<>();
+        cities.add("Кіровоград");
+        cities.add("Полтава");
+        cities.add("Одеса");
+        cities.add("Харків");
+        System.out.println(cities);
+    }
+}
